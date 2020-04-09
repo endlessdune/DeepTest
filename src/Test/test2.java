d99848091ffddcf99003f3133a17bec20a488a8a
endlessdune <48548503+endlessdune@users.noreply.github.com>
2020-04-09 17:38:27 +0800

    second commit

---
+++ src/Test/test2.java
@@ -0,0 +1,10 @@
+import util.*;
+
+public calss Read {
+    public static void main(String[] args) {
+        String str = "what is it";
+        for (int i = 0; i < str.length() + 1; i++) {
+            char s = str[i];
+        }
+    }
+}
